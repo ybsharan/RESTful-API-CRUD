4edc998d3ecb466fcd33398c583d84e2db9641e5
ybsharan <bsharanyadav@gmail.com>
2023-05-24 18:37:55 +0530

    access control update

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.use(
 
 app.get('/', (req, res) => {
   res.send('Nice Working');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Max-Age', '1800');
+  res.setHeader('Access-Control-Allow-Headers', 'content-type');
 });
 
 //using error handler
